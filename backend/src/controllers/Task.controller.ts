25f90d0c6fe5f143fc15b92c5aa84ff87c10c567
caiooliveirah09 <leelros@hotmail.com>
2023-06-16 20:15:09 -0300

    feat: add task controller and create, get tasks

---
+++ backend/src/controllers/Task.controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import TaskService from "../services/Task.service";
+
+export default class TaskController {
+  private service: TaskService;
+
+  constructor(
+    private req: Request,
+    private res: Response,
+    private next: NextFunction
+  ) {
+    this.service = new TaskService();
+  }
+
+  public async createNewTask() {
+    const { body } = this.req;
+    const { status, message } = await this.service.createNewTask(body);
+    return this.res.status(status).json(message);
+  }
+
+  public async getAllTasks() {
+    const { body } = this.req;
+    const { status, message } = await this.service.getAllTasks(body);
+    return this.res.status(status).json(message);
+  }
+  /*
+  public async updateOneTask() {
+    const { id, ...update } = this.req.body;
+    const { status, message } = await this.service.updateOneTask(id, update);
+    return this.res.status(status).json(message);
+  }
+
+  public async deleteOneTask() {
+    const { id } = this.req.body;
+    const { status, message } = await this.service.deleteOneTask(id);
+    return this.res.status(status).json(message);
+  }
+  */
+}
